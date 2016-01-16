b19156a4c2cf4fc1ae8b3e5cd61bc82652d5ace0
Kerry Toonen <kerrytoonen@gmail.com>
2016-01-16 15:02:18 -0500

    Initial Commit

---
+++ Patient_Doctor2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Patient_Doctor2
+//
+//  Created by Kerry Toonen on 2016-01-16.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
